0815ba12c0b737a3d23516e503e42cc751cbf53d
Josh Beveridge <joshua.beveridge@icloud.com>
2014-12-04 13:27:07 -0400

    Basic Structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// OnClick =====================================================================
+
+	$('.person').click(function() {
+
+		if($(this).hasClass('active')) {
+			$(this).removeClass('active');
+		}
+		else {
+			$(this).addClass('active');
+		}
+
+	});
